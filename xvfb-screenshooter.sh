155bdb70d32a7e0b21108ce12e90ab165971aa83
Takashi Masuyama <mamewotoko@gmail.com>
2020-01-05 14:31:09 +0900

    write resolution suitable to youtube

--- xvfb-screenshooter.sh
+++ xvfb-screenshooter.sh
@@ -6,6 +6,14 @@ set -e
 
 # do not change this
 DISPLAY_BAK=$DISPLAY
+
+# 2160p: 3840x2160
+# 1440p: 2560x1440
+# 1080p: 1920x1080
+# 720p: 1280x720
+# 480p: 854x480
+# 360p: 640x360
+# 240p: 426x240
 if [ -z "$DIPLAY_SIZE" ]; then
     # HD
     DISPLAY_SIZE=1280x720
